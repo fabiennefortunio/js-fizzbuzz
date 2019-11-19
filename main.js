3f4f9d077da239447a4a25d853907d90114b6950
Fabienne <fortunio.fabienne@gmail.com>
2019-11-19 15:26:13 +0100

    finito

---
+++ main.js
@@ -0,0 +1,16 @@
+/*Stampare tutti i numeri da 1 a 100 con le seguenti regole:
+al posto dei multipli di 3 stampare "Fizz"
+al posto dei multipli di 5 stampare "Buzz"
+al posto dei multipli di 3 e 5 stampare "FizzBuzz"*/
+
+for (var i=1; i < 100; i++)
+{
+    if (i % 15 == 0)
+        console.log('FizzBuzz');
+    else if (i % 3 == 0)
+        console.log('Fizz');
+    else if (i % 5 == 0)
+        console.log('Buzz');
+    else
+        console.log(i);
+}
